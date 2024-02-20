b1b0188495b792b3d6e56a3985eee1ac0bd431eb
Mohana <97424463+21A91A05E6@users.noreply.github.com>
2024-02-20 14:08:08 +0530

    Time: 14 ms (27.95%) | Memory: 13.8 MB (30.85%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -5,12 +5,10 @@ public:
         int c=0,a=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(i+j==n-1){
+                if(i+j==n-1 || i==j){
                     c+=mat[i][j];
                 }
-                else if(i==j){
-                    c+=mat[i][j];
-                }
+                
                 // ((n-(n/2))+1)
                 // if(n%2!=0 && i==j && i==n/2 && j==n/2){
                 //     a=mat[i][j];
